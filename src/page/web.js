746f37f4045d2346657fb46798d379554955fad2
G-shitou <gshitou@126.com>
2020-03-06 09:08:46 +0800

    webview与主题关联

--- src/page/web.js
+++ src/page/web.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
     StyleSheet,
     ProgressViewIOS,
@@ -8,7 +9,7 @@ import {
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-export default class Web extends React.Component {
+class Web extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -19,15 +20,15 @@ export default class Web extends React.Component {
         const { navigation,route } = this.props;
         const url = route.params.url;
         return (
-            <View style={styles.container}>
+            <View style={[styles.container,{backgroundColor:this.props.theme.backgroundColor}]}>
                 {/* <View style={styles.titleBar}/> */}
                 {this.state.progress !== 1 && (Platform.OS === 'IOS' ? <ProgressViewIOS
                     //这是进度条颜色
-                    progressTintColor="#2D92FF"
-                    progress={this.state.progress}/> : <ProgressBarAndroid styleAttr="Horizontal" color="#2D92FF" progress={this.state.progress}/>)}
+                    progressTintColor={this.props.theme.themeColor}
+                    progress={this.state.progress}/> : <ProgressBarAndroid styleAttr="Horizontal" color={this.props.theme.themeColor} progress={this.state.progress}/>)}
                 <WebView
                     source={{ uri: url }}
-                    style={{ flex: 1 }}
+                    style={{ flex: 1,backgroundColor:this.props.theme.backgroundColor}}
                     //设置进度 progress值为0～1
                     onLoadProgress={({nativeEvent}) => this.setState(
                         {progress: nativeEvent.progress}
@@ -37,7 +38,12 @@ export default class Web extends React.Component {
         );
     }
 };
-
+const mapStateToProps = state => {
+    return {
+        theme: state.themeReducer.theme
+    }
+  }
+export default connect(mapStateToProps)(Web)
 const styles = StyleSheet.create({
     container:{
         flex:1
